dd2bce8e028ceeac7b0727ae4c8f43ece24b42d3
darklordsadaf <darklordsadaf@gmail.com>
2020-08-12 04:35:14 +0600

    setTimeout done

---
+++ timeout.js
@@ -0,0 +1,23 @@
+/**-----setTimeout--------- */
+console.log(2222);
+setTimeout(function(){
+    console.log(3333);
+},2000);
+console.log(4444);
+
+
+/**-----setTimeout with array function--------- */
+// console.log(2222);
+// setTimeout(() => {
+//     console.log(3333);
+// },2000);
+// console.log(4444);
+
+
+
+/**-----setInterval--------- */
+// console.log(2222);
+// setInterval(function(){
+//     console.log(3333);
+// },2000);
+// console.log(4444);
