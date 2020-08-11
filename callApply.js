5ac25e667bfc5d9f4cac228eb14140fa7869fe0c
darklordsadaf <darklordsadaf@gmail.com>
2020-08-11 16:27:05 +0600

    bind done

--- callApply.js
+++ callApply.js
@@ -6,13 +6,36 @@ const normalPerson = {
         console.log(this.firstName, this.lastName);
     },
     chargeBill: function(amount){
+        console.log(this);
         this.salary = this.salary - amount;
         return this.salary;
     }
 }
 
+const heroPerson = {
+    firstName: "Ronald",
+    lastName: "Mac",
+    salary: 25000
+}
+
+const zeroPerson = {
+    firstName: "Tom",
+    lastName: "Holland",
+    salary: 27000
+}
+
+
+const heroPersonChargeBill = normalPerson.chargeBill.bind(heroPerson);
+heroPersonChargeBill(2000);
+console.log(heroPerson.salary);
+
+const zeroPersonChargeBill = normalPerson.chargeBill.bind(zeroPerson);
+zeroPersonChargeBill(10000);
+console.log(zeroPerson.salary);
+
+
+
 normalPerson.chargeBill(150);
 normalPerson.chargeBill(3000);
 console.log(normalPerson.salary);
-// console.log(normalPerson.salary);
-console.log(normalPerson.firstName);
+// console.log(normalPerson.firstName);
