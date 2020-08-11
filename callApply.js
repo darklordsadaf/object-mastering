180bc5a7be3b9b5117e78208f50782769044b302
darklordsadaf <darklordsadaf@gmail.com>
2020-08-11 18:47:26 +0600

    call-apply-done

--- callApply.js
+++ callApply.js
@@ -1,3 +1,4 @@
+/**----------------- apply -----------------*/
 const normalPerson = {
     firstName: "Ron",
     lastName: "Dash",
@@ -5,9 +6,9 @@ const normalPerson = {
     getFullName: function(){
         console.log(this.firstName, this.lastName);
     },
-    chargeBill: function(amount){
+    chargeBill: function(amount, tips, tax){
         console.log(this);
-        this.salary = this.salary - amount;
+        this.salary = this.salary - amount - tips - tax;
         return this.salary;
     }
 }
@@ -24,18 +25,92 @@ const zeroPerson = {
     salary: 27000
 }
 
-
-const heroPersonChargeBill = normalPerson.chargeBill.bind(heroPerson);
-heroPersonChargeBill(2000);
+normalPerson.chargeBill.apply(heroPerson, [1000, 100, 10]);
+normalPerson.chargeBill.apply(heroPerson, [4000, 400, 40]);
 console.log(heroPerson.salary);
 
-const zeroPersonChargeBill = normalPerson.chargeBill.bind(zeroPerson);
-zeroPersonChargeBill(10000);
-console.log(zeroPerson.salary);
-
-
-
-normalPerson.chargeBill(150);
-normalPerson.chargeBill(3000);
 console.log(normalPerson.salary);
-// console.log(normalPerson.firstName);
+
+
+
+/**------------------ call -----------------------*/
+// const normalPerson = {
+//     firstName: "Ron",
+//     lastName: "Dash",
+//     salary: 15000,
+//     getFullName: function(){
+//         console.log(this.firstName, this.lastName);
+//     },
+//     chargeBill: function(amount, tips, tax){
+//         console.log(this);
+//         this.salary = this.salary - amount - tips - tax;
+//         return this.salary;
+//     }
+// }
+
+// const heroPerson = {
+//     firstName: "Ronald",
+//     lastName: "Mac",
+//     salary: 25000
+// }
+
+// const zeroPerson = {
+//     firstName: "Tom",
+//     lastName: "Holland",
+//     salary: 27000
+// }
+
+// normalPerson.chargeBill.call(heroPerson, 5000, 500, 50);
+// normalPerson.chargeBill.call(heroPerson, 2000, 200, 20);
+// console.log(heroPerson.salary);
+
+// console.log(normalPerson.salary);
+
+
+
+
+
+
+
+/**------------- bind -------------------------*/
+// const normalPerson = {
+//     firstName: "Ron",
+//     lastName: "Dash",
+//     salary: 15000,
+//     getFullName: function(){
+//         console.log(this.firstName, this.lastName);
+//     },
+//     chargeBill: function(amount){
+//         console.log(this);
+//         this.salary = this.salary - amount;
+//         return this.salary;
+//     }
+// }
+
+// const heroPerson = {
+//     firstName: "Ronald",
+//     lastName: "Mac",
+//     salary: 25000
+// }
+
+// const zeroPerson = {
+//     firstName: "Tom",
+//     lastName: "Holland",
+//     salary: 27000
+// }
+
+
+// const heroPersonChargeBill = normalPerson.chargeBill.bind(heroPerson);
+// heroPersonChargeBill(2000);
+// console.log(heroPerson.salary);
+
+// const zeroPersonChargeBill = normalPerson.chargeBill.bind(zeroPerson);
+// zeroPersonChargeBill(10000);
+// console.log(zeroPerson.salary);
+
+
+
+// normalPerson.chargeBill(150);
+// normalPerson.chargeBill(3000);
+// console.log(normalPerson.salary);
+// // console.log(normalPerson.firstName);
